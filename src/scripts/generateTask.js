531ca12bbe5b913419e81c279690e8ac5e5b67ca
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-01 15:55:57 -0300

    feat: input funcionando e adicionando os valores a um array

---
+++ src/scripts/generateTask.js
@@ -0,0 +1,37 @@
+const btnAdd = document.querySelector("#button-Add");
+const textBoxAdd = document.querySelector("#todoapp-addTask");
+const sectionAdd = document.querySelector(".tasksList-listTasks");
+let tasksList = [];
+
+btnAdd.addEventListener("click", () => {
+  if (textBoxAdd.value === "") {
+    window.alert("[ERRO] Você não digitou nenhuma tarefa!");
+  } else {
+    tasksList.push(textBoxAdd.value);
+    
+    sectionAdd.innerHTML += `
+    <li class="tasksList-task">
+    <div>
+    <input
+    type="checkbox"
+    name="taskCheck"
+        id="task${tasksList.length}"
+        class="tasksList-taskCheck"
+        />
+        <label for="task1">${textBoxAdd.value}</label>
+        </div>
+        <div>
+        <button
+        type="button"
+        class="todo-button fa-solid fa-pen-to-square"
+        ></button>
+        <button
+        type="button"
+        class="todo-button fa-solid fa-trash"
+        ></button>
+        </div>
+        </li>
+        `;
+    }
+    textBoxAdd.value = "";
+});
