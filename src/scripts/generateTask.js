a0c083fe2d4bee193ffd61b658f76b2dfa92f31b
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-08 14:20:22 -0300

    feat: finalizei a funcionalidade com js

--- src/scripts/generateTask.js
+++ src/scripts/generateTask.js
@@ -1,43 +1,106 @@
-const btnAdd = document.querySelector("#button-Add");
+const formAdd = document.querySelector("#add-form");
+const formEdit = document.querySelector("#edit-form");
 const textBoxAdd = document.querySelector("#todoapp-addTask");
-const sectionAdd = document.querySelector(".tasksList-listTasks");
-let tasksList = [];
+const textBoxEdit = document.querySelector("#todoapp-editTask");
+const tasksList = document.querySelector(".tasksList-listTasks");
+const btnAdd = document.querySelector("#button-Add");
+const btnEdit = document.querySelector("#button-Edit");
+const btnCancelEdit = document.querySelector("#button-Cancel");
 
-btnAdd.addEventListener("click", () => {
-  if (textBoxAdd.value === "") {
-    window.alert("[ERRO] Você não digitou nenhuma tarefa!");
-  } else {
-    tasksList.push(textBoxAdd.value);
+let oldInputValue;
 
-    sectionAdd.innerHTML += `
+const saveToDo = (text) => {
+  tasksList.innerHTML += `
     <li class="tasksList-task">
       <div class="task-LeftSide">
-        <span name="taskCheck"
-        id="task${tasksList.length}"
-        class="tasksList-taskCheck">
-          <i class="fa-solid"></i> 
-          <!--fa-check-->
+        <span class="tasksList-taskCheck">
+          <i class="fa-solid"></i>
         </span>
-        <label for="task${tasksList.length}" id="label-Task${tasksList.length}" class="done">${textBoxAdd.value}</label>
+        <p>${text}</p>
       </div>
-      <div>
+      <div class="task-RightSide">
         <button
-        type="button"
-        class="todo-button fa-solid fa-pen-to-square"
+          type="button"
+          class="todo-button fa-solid fa-pen-to-square tasksList-taskEdit"
         ></button>
         <button
-        type="button"
-        class="todo-button fa-solid fa-trash"
+          type="button"
+          class="todo-button fa-solid fa-trash tasksList-taskRemove"
         ></button>
       </div>
     </li>
-        `;
+    `;
+};
+
+const toggleForms = () => {
+  formEdit.classList.toggle("hide");
+  formAdd.classList.toggle("hide");
+  tasksList.classList.toggle("hide");
+};
+
+const updateToDo = (text) => {
+  const allTasks = document.querySelectorAll(".tasksList-task");
+
+  allTasks.forEach((task) => {
+    let taskText = task.querySelector("p")
+
+    if(taskText.innerText === oldInputValue) {
+      taskText.innerText = text
+    }
+  })
+}
+
+formAdd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = textBoxAdd.value;
+  if (inputValue) {
+    saveToDo(inputValue);
+    textBoxAdd.value = "";
+    textBoxAdd.focus();
   }
-  textBoxAdd.value = "";
 });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    btnAdd.click();
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("li")
+  const pEl = targetEl.closest("div").querySelector("p");
+  let taskTitle;
+
+  if (parentEl && parentEl.querySelector("p")) {
+    taskTitle = parentEl.querySelector("p").innerHTML
+  }
+
+  if (targetEl.classList.contains("tasksList-taskCheck")) {
+    targetEl.classList.toggle("fa-check");
+    pEl.classList.toggle("done");
+  }
+
+  if (targetEl.classList.contains("tasksList-taskRemove")) {
+    targetEl.closest("li").remove();
+  }
+
+  if (targetEl.classList.contains("tasksList-taskEdit")) {
+    toggleForms();
+
+    textBoxEdit.value = taskTitle
+    oldInputValue = taskTitle
   }
 });
+
+btnCancelEdit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleForms();
+});
+
+formEdit.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const editInputValue = textBoxEdit.value
+
+  if (editInputValue) {
+    updateToDo(editInputValue)
+  }
+
+  toggleForms()
+})
